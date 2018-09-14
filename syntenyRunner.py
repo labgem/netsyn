f0ca8853b88a7dea39491c86193806fa88ff8348
Celine Chevalier <cchevali@genoscope.cns.fr>
2018-09-14 15:49:01 +0200

    ADD: synteny filtration and scoring

--- syntenyRunner.py
+++ syntenyRunner.py
@@ -4,11 +4,12 @@ import json
 import subprocess
 
 MICSYNTENY_CP = os.environ.get('MICSYNTENY_CP')
-GC_SIZE_USER = 3
+GC_SIZE_USER = 5
 HALF_SIZE_USER = int(GC_SIZE_USER/2)
-GAP_USER = GC_SIZE_USER
+GAP_USER = 3
 MIN_SYNTENY_SIZE = 1
-FILTERING = "OFF"
+FILTERING = "ON"
+analysis_id = "414"
 
 def get_target_list(lines):
     target_list = []
@@ -116,7 +117,7 @@ def write_ALL_nodes(gc_user_dict, posRel_dict):
                             str(start),
                             str(stop),
                             frame,
-                            "414"]))
+                            analysis_id]))
                         file.write("\n")
     return 0
 
@@ -134,20 +135,18 @@ def store_roles(real_fam_dict, cds_target_dict):
                 for atarget1 in target1:
                     for atarget2 in target2:
                         roles_storage.append((INC_roles, member1, atarget1,
-                                             member2, atarget2))
+                                              member2, atarget2))
                         INC_roles += 1
     return roles_storage
-    
-
 
 def write_ALL_roles(roles_storage):
     with open("ALL.roles", "w") as file:
         for arole in roles_storage:
             file.write(" ".join([
-                        str(arole[0]),
-                        "@".join([arole[1], arole[2]]),
-                        "@".join([arole[3], arole[4]])
-                        ]))
+                str(arole[0]),
+                "@".join([arole[1], arole[2]]),
+                "@".join([arole[3], arole[4]])
+                ]))
             file.write("\n")
     return 0
 
@@ -160,7 +159,179 @@ def run_synteny():
                        stdout=file, stderr=file)
     return 0
 
+def get_cpd_role_synton(lines):
+    role_synton_cpd = {}
+    role_synton_cpd["roles"] = {}
+    role_synton_cpd["syntons"] = {}
+    for aline in lines:
+        aline = aline.strip().split("$")
+        role, synton = aline
+        role_synton_cpd["roles"][role] = synton
+        role_synton_cpd["syntons"].setdefault(synton, []).append(role)
+    print("role_synton_cpd :", role_synton_cpd)
+    return role_synton_cpd
 
+def synteny_filtering_preparation(posRel_dict, roles_storage, role_synton_cpd):
+    pos_in_synteny = []
+    for asynteny in role_synton_cpd["syntons"]:
+        for aroleID in role_synton_cpd["syntons"][asynteny]:
+            role = [arole for arole in roles_storage if str(arole[0]) == aroleID]
+            cds1, target1, cds2, target2 = role[0][1:]
+            for atarget in posRel_dict:
+                if atarget == target1:
+                    for cds, position in posRel_dict[atarget]:
+                        if cds == cds1:
+                            pos_cds1 = position
+                        if cds == target1:
+                            pos_target1 = position
+                if atarget == target2:
+                    for cds, position in posRel_dict[atarget]:
+                        if cds == cds2:
+                            pos_cds2 = position
+                        if cds == target2:
+                            pos_target2 = position
+            pos_in_synteny.append((asynteny, pos_cds1, pos_target1, pos_cds2, pos_target2))
+    return pos_in_synteny
+
+def min_max_synteny(pos_in_synteny):
+    synteny_description = {}
+    for asynteny in pos_in_synteny:
+        synt_id = asynteny[0]
+        pos_gene_1 = asynteny[1]
+        pos_target_gene_1 = asynteny[2]
+        pos_gene_2 = asynteny[3]
+        pos_target_gene_2 = asynteny[4]
+
+        if not synt_id in synteny_description:
+            synteny_description[synt_id] = {'minG1' : pos_gene_1,
+                                            'maxG1' : pos_gene_1,
+                                            'targetG1' : pos_target_gene_1,
+                                            'minG2' : pos_gene_2,
+                                            'maxG2' : pos_gene_2,
+                                            'targetG2' : pos_target_gene_2}
+        else:
+            # Genome 1 in synteny
+            if pos_gene_1 < synteny_description[synt_id]['minG1']:
+                synteny_description[synt_id]['minG1'] = pos_gene_1
+            elif pos_gene_1 > synteny_description[synt_id]['maxG1']:
+                synteny_description[synt_id]['maxG1'] = pos_gene_1
+            # Genome 2 in synteny
+            if pos_gene_2 < synteny_description[synt_id]['minG2']:
+                synteny_description[synt_id]['minG2'] = pos_gene_2
+            elif pos_gene_2 > synteny_description[synt_id]['maxG2']:
+                synteny_description[synt_id]['maxG2'] = pos_gene_2
+    return synteny_description
+
+def genome_conserved(start, end, target, gap):
+    if target >= start and target <= end:
+        conserved = True                        # .....start....target....end.....
+    elif target < start:
+        # pas besoin du if-else:
+        # conserved = (start-target-1) <= gap
+        if (start-target-1) <= gap:
+            conserved = True                    # .....target.start....end.....
+        else:
+            conserved = False                   # .....target...........start....end.....
+    elif target > end:
+        # idem
+        # conserved = (target-end-1) <= gap
+        if (target-end-1) <= gap:
+            conserved = True                    # .....start.....end.target.....
+        else:
+            conserved = False                   # .....start.....end...........target.....
+    return conserved
+
+def check_target_ourside_synteny(mini, maxi, target):
+    if target < mini:
+        mini = target
+    elif target > maxi:
+        maxi = target
+    return mini, maxi
+
+def synteny_purification(synteny_description, gap, filtering):
+    synteny_conserved = []
+    synteny_not_conserved = []
+    for synt_id in synteny_description:
+        minG1 = synteny_description[synt_id]['minG1']
+        maxG1 = synteny_description[synt_id]['maxG1']
+        targetG1 = synteny_description[synt_id]['targetG1']
+        minG2 = synteny_description[synt_id]['minG2']
+        maxG2 = synteny_description[synt_id]['maxG2']
+        targetG2 = synteny_description[synt_id]['targetG2']
+
+        if filtering == 'on':
+            g1 = genome_conserved(minG1, maxG1, targetG1, gap)
+            g2 = genome_conserved(minG2, maxG2, targetG2, gap)
+        else:
+            g1 = True
+            g2 = True
+
+        minG1, maxG1 = check_target_ourside_synteny(minG1, maxG1, targetG1)
+        minG2, maxG2 = check_target_ourside_synteny(minG2, maxG2, targetG2)
+
+        if g1 and g2:
+            synteny_conserved.append([synt_id, minG1, maxG1, minG2, maxG2])
+        else:
+            synteny_not_conserved.append([synt_id, minG1, maxG1, minG2, maxG2])
+
+    return synteny_conserved, synteny_not_conserved
+
+def list_writing(list, output_name, separator):
+    with open(output_name, 'w') as file:
+        for line in list:
+            line = separator.join(str(e) for e in line)
+            file.write(line+'\n')
+
+def outputs_writing(synteny_conserved, synteny_not_conserved, separator):
+    list_writing(synteny_conserved, 'synteny_conserved.filtered', separator)
+    list_writing(synteny_not_conserved, 'synteny_not_conserved.filtered', separator)
+
+def filter_synteny(positions_in_synteny):
+    # Get min and max relative positions for Genome 1 and 2 in synteny
+    synteny_description = min_max_synteny(positions_in_synteny)
+    # Check filter
+    synteny_conserved, synteny_not_conserved = synteny_purification(synteny_description, GAP_USER, FILTERING)
+    # Outputs writing
+    separator = "$"
+    outputs_writing(synteny_conserved, synteny_not_conserved, separator)
+    return synteny_conserved
+
+def get_syntons_info(synton_db):
+    synton_desc = {}
+    for aline in synton_db:
+        aline = aline.strip().split("$")
+        synt_id = aline[0]
+        analysis_id = aline[1]
+        startG1 = aline[3]
+        stopG1 = aline[4]
+        startG2 = aline[5]
+        stopG2 = aline[6]
+        nbgeneG1 = aline[7]
+        nbgeneG2 = aline[8]
+        synton_desc[synt_id] = [
+            synt_id,
+            analysis_id,
+            startG1,
+            stopG1,
+            startG2,
+            stopG2,
+            nbgeneG1,
+            nbgeneG2
+            ]
+    return synton_desc
+
+def score_calculation(syntenies_list, synton_desc):
+    syntenies_scores = {}
+    for asynt_id in synton_desc:
+        prot_in_synt = int(synton_desc[asynt_id][6]) + int(synton_desc[asynt_id][7])
+        startG1, stopG1, startG2, stopG2 = [pos for pos in syntenies_list if pos[0] == asynt_id][0][1:]
+        synteny_rangeG1 = (int(stopG1)-int(startG1))+1
+        synteny_rangeG2 = (int(stopG2)-int(startG2))+1
+        mean_score = prot_in_synt/2
+        weighted_score = prot_in_synt / (synteny_rangeG1 + synteny_rangeG2)
+        syntenies_scores.setdefault(asynt_id, {}).setdefault("mean", mean_score)
+        syntenies_scores[asynt_id]["weighted"] = weighted_score
+    return syntenies_scores
 
 def main(gcFile, families, targets):
     with open(targets, "r") as target_data:
@@ -176,13 +347,28 @@ def main(gcFile, families, targets):
     real_fam_dict = {key:value for (key, value) in fam_user_dict.items() if len(value) > 1}
     posRel_dict = set_relative_position(cds_target_dict, gc_user_dict)
     roles_storage = store_roles(real_fam_dict, cds_target_dict)
-    print(roles_storage)
 
     write_ALL_nodes(gc_user_dict, posRel_dict)
     write_ALL_roles(roles_storage)
     run_synteny()
 
-    
+    with open("ALL.CPD.DB", "r") as cpd_id_roles_synton:
+        id_roles_synton = cpd_id_roles_synton.readlines()
+    role_synton_cpd = get_cpd_role_synton(id_roles_synton)
+
+    positions_in_synteny = synteny_filtering_preparation(posRel_dict, roles_storage, role_synton_cpd)
+    conserved_synteny = filter_synteny(positions_in_synteny)
+
+    with open("ALL.SYNTON.DB", "r") as synton_db:
+        short_synton_db = synton_db.readlines()
+    synton_desc = get_syntons_info(short_synton_db)
+
+    syntenies_scores = score_calculation(conserved_synteny, synton_desc)
+    print(syntenies_scores)
+    #write_scores(mean_score, weighted_score,) # pas utile d'éditer le fichier
+                                               # ALL.SYNTON.DB car pas de
+                                               # nouvelle entrée dans la base
+                                               # de données
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2], sys.argv[3])
